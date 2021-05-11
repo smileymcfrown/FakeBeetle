da9177abc98c0ae668605ff9fb632f42271d25f5
smileymcfrown <46418167+smileymcfrown@users.noreply.github.com>
2021-05-12 03:34:41 +1000

    Moved unused assets and scenes to scenes/unused
    Also added comments to MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,13 +8,14 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        LoadSave.Load();
+        // Load all the levels from file at start - could have been done from Levels.cs - done here
+        // as a "Custom Game" menu item was to be made in which the player could re-arrange
+        // the level to play their own custom levels in any order.
+
+        LoadSave.Load(); 
         //PlayerData.player = new PlayerData();
 
-
     }
-
-
 
     public void StartGame() // Loads The Level selector once the "Start" Button is clicked.
     {
